20c6a0d02a8839019718be625591322cc25c9dff
Shangtong Zhang <zhangshangtong.cpp@foxmail.com>
2016-03-30 17:00:43 +0800

    chrome-like style

--- js/ObjectInspector.js
+++ js/ObjectInspector.js
@@ -10,18 +10,36 @@ ObjectInspector.prototype.init = function() {
   var me = this;
   this._$tree.bind('tree.click', function(event) {
     var node = event.node;
-    if (me._status[node.id] === 'TO_OPEN') {
-      var obj = me._objCache[node.id];
-      if (me.hasSubNodes(obj)) {
-        var subNodes = me.format(me._objCache[node.id]);
-        for (var i = 0; i < subNodes.length; ++i) {
-          me._$tree.tree('appendNode', subNodes[i], node);
-        }
-        me._status[node.id] = 'DONE';
-      }
-    }
+    me.loadNode(node);
     me._$tree.tree('toggle', node);
   });
+  this._$tree.bind('tree.open', function(event) {
+    me.loadNode(event.node);
+  });
+};
+ObjectInspector.prototype.loadNode = function(node) {
+  if (!node) {
+    return;
+  }
+  if (this._status[node.id] === 'TO_OPEN') {
+    if (node.children && node.children[0] && node.children[0].id === 'dummy') {
+      this._$tree.tree('removeNode', node.children[0]);
+    }
+    var obj = this._objCache[node.id];
+    if (this.hasSubNodes(obj)) {
+      var subNodes = this.format(this._objCache[node.id]);
+      for (var i = 0; i < subNodes.length; ++i) {
+        var subNode = subNodes[i];
+        this._$tree.tree('appendNode', subNode, node);
+        if (subNode.children) {
+          for (var j = 0; j < subNode.children.length; ++j) {
+            this._$tree.tree('appendNode', subNode.children[j], this._$tree.tree('getNodeById', subNode.id));
+          }
+        }
+      }
+      this._status[node.id] = 'DONE';
+    }
+  }
 };
 ObjectInspector.prototype.show = function() {
   var children = [];
@@ -42,11 +60,34 @@ ObjectInspector.prototype.show = function() {
   }
   this._$tree.tree({
     data: [{
-      label: label, 
+      label: '',
+      tag: null,
+      value: label,
       id: this.getId(),
       children: children
     }],
-    autoOpen: true
+    autoOpen: false,
+    onCreateLi: function(node, $li) {
+      $nodeDiv = $li.children('div');
+      $nodeDiv.addClass('objInsEntry');
+      $tag = $nodeDiv.children('span');
+      $sep = $tag.clone().css('margin-left', '0').addClass('objInsEntrySep');
+      $value = $tag.clone().css('margin-left', '0').addClass('objInsEntryValue');
+      if (node.type) {
+        $value.attr('type', node.type);
+      }
+      $tag.addClass('objInsEntryTag');
+      $sep.appendTo($nodeDiv);
+      $value.appendTo($nodeDiv);
+      if (node.tag === null) {
+        $tag.text('');
+        $sep.text('');
+      } else {
+        $tag.text(node.tag);
+        $sep.text(': ');
+      }
+      $value.text(node.value);
+    }
   });
 };
 ObjectInspector.prototype.getId = function() {
@@ -57,22 +98,26 @@ ObjectInspector.prototype.hasSubNodes = function(obj) {
   return $.isPlainObject(obj) || $.isArray(obj);
 };
 ObjectInspector.prototype.createLabel = function(obj, prop) {
+  var label = {
+    tag: String(prop)
+  };
   if (!this.hasSubNodes(obj[prop])) {
     var valueStr;
     if (typeof obj[prop] === 'string') {
       valueStr = '"' + obj[prop] + '"';
     } else {
-      valueStr = obj[prop];
+      valueStr = String(obj[prop]);
     }
-    return prop.toString() + ': ' + valueStr;
+    label.value = valueStr;
   } else {
     if ($.isPlainObject(obj[prop])) {
-      return prop.toString() + ': Object';
+      label.value = 'Object';
     }
     if ($.isArray(obj[prop])) {
-      return prop.toString() + ': Array[' + obj[prop].length + ']';
+      label.value = 'Array[' + obj[prop].length + ']';
     }
   }
+  return label;
 };
 ObjectInspector.prototype.format = function(obj) {
   var me = this;
@@ -80,13 +125,22 @@ ObjectInspector.prototype.format = function(obj) {
   for (var prop in obj) {
     if (obj.hasOwnProperty(prop)) {
       var id = this.getId();
+      var label = me.createLabel(obj, prop);
       var childNode = {
-        label: me.createLabel(obj, prop),
-        id: id
+        label: '',
+        id: id,
+        tag: label.tag,
+        value: label.value,
+        type: typeof obj[prop]
       };
-      // if ($.isPlainObject(obj[prop])) {
-      // } else if ($.isArray(obj[prop])) {
-      // }
+      if (this.hasSubNodes(obj[prop])) {
+        childNode.children = [{
+          label: '',
+          id: 'dummy',
+          tag: null,
+          value: ''
+        }];
+      }
       propArr.push(childNode);
       me._status[id] = 'TO_OPEN';
       me._objCache[id] = obj[prop];
